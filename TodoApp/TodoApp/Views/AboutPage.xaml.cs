a10b9d4d71bcf3210e7a0d97c30ac0fad0582d17
nw341 <nathan.wang2@gmail.com>
2017-05-07 22:50:21 -0500

    First big push for application

---
+++ TodoApp/TodoApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace TodoApp.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
